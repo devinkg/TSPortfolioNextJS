48fd354d2e6413539d09d9e5fba009845b03af51
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-03-22 01:09:11 +0530

    navbar somewhat completed

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,75 @@
-import React from "react";
+"use client";
+
+import Link from "next/link";
+import React, { useState } from "react";
+
+const links = [
+  { url: "/", title: "Home" },
+  { url: "/about", title: "About" },
+  { url: "/portfolio", title: "Portfolio" },
+  { url: "/contact", title: "Contact" },
+];
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div
+      className={
+        "h-full flex items-center justify-between px-4 sm:px-8 md:px-12 lg:px-20 xl:px-48"
+      }
+    >
+      <div className={"hidden md:flex gap-4"}>
+        {links.map((link) => (
+          <Link href={link.url} key={link.title} className="">
+            {link.title}
+          </Link>
+        ))}
+      </div>
+      {/* LOGO */}
+      <div className={"md:hidden"}>
+        <Link
+          href={"/"}
+          className="text-sm bg-black rounded-md p-1 font-semibold flex justify-center items-center"
+        >
+          <span className={"text-white mx-1"}>Devin</span>
+          <span
+            className={
+              "w-16 h-8 rounded bg-white text-black flex justify-center items-center"
+            }
+          >
+            .Human
+          </span>
+        </Link>
+      </div>
+      {/* RESPONSIVE UI */}
+      <div className={"md:hidden"}>
+        {/* MENU BUTTON */}
+        <button
+          className={"w-10 h-8 flex flex-col justify-between z-50 relative"}
+          onClick={() => setOpen((prevState) => !prevState)}
+        >
+          <div className={"w-10 h-1 bg-white rounded"}></div>
+          <div className={"w-10 h-1 bg-white rounded"}></div>
+          <div className={"w-10 h-1 bg-white rounded"}></div>
+        </button>
+        {/*  MENU LIST */}
+        {open && (
+          <div
+            className={
+              "absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl"
+            }
+          >
+            {links.map((link) => (
+              <Link href={link.url} key={link.title}>
+                {link.title}
+              </Link>
+            ))}
+          </div>
+        )}
+      </div>
+    </div>
+  );
 };
 
 export default NavBar;
