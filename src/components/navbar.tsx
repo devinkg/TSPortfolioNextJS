610d679f40741b63d97bc15c2b68ca4609e2850c
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-03-25 00:49:29 +0530

    animation added to hamburger menu page contents

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import React, { useState } from "react";
 import NavLink from "./navLink";
+import { motion } from "framer-motion";
 
 const links: Array<LinksTypes> = [
   { url: "/", title: "Home" },
@@ -15,6 +16,59 @@ const links: Array<LinksTypes> = [
 
 const NavBar = () => {
   const [open, setOpen] = useState(false);
+
+  const topVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: 45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+
+  const centerVariants = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  };
+
+  const bottomVariants = {
+    closed: {
+      rotate: 0,
+    },
+    opened: {
+      rotate: -45,
+      backgroundColor: "rgb(255,255,255)",
+    },
+  };
+
+  const listVariants = {
+    closed: {
+      x: "100vw",
+    },
+    opened: {
+      x: 0,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.2,
+      },
+    },
+  };
+
+  const listItemVariants = {
+    closed: {
+      x: -10,
+      opacity: 0,
+    },
+    opened: {
+      x: 0,
+      opacity: 1,
+    },
+  };
 
   return (
     <div
@@ -72,23 +126,38 @@ const NavBar = () => {
           className={"w-10 h-8 flex flex-col justify-between z-50 relative"}
           onClick={() => setOpen((prevState) => !prevState)}
         >
-          <div className={"w-10 h-1 bg-white rounded"}></div>
-          <div className={"w-10 h-1 bg-white rounded"}></div>
-          <div className={"w-10 h-1 bg-white rounded"}></div>
+          <motion.div
+            variants={topVariants}
+            animate={open ? "opened" : "closed"}
+            className={"w-10 h-1 bg-black rounded origin-left"}
+          ></motion.div>
+          <motion.div
+            variants={centerVariants}
+            animate={open ? "opened" : "closed"}
+            className={"w-10 h-1 bg-black rounded"}
+          ></motion.div>
+          <motion.div
+            variants={bottomVariants}
+            animate={open ? "opened" : "closed"}
+            className={"w-10 h-1 bg-black rounded origin-left"}
+          ></motion.div>
         </button>
         {/*  MENU LIST */}
         {open && (
-          <div
+          <motion.div
+            variants={listVariants}
+            initial={"closed"}
+            animate={"opened"}
             className={
-              "absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl"
+              "absolute top-0 left-0 w-screen h-screen bg-black text-white flex flex-col justify-center items-center gap-8 text-4xl z-40"
             }
           >
             {links.map((link: LinksTypes) => (
-              <Link href={link.url} key={link.title}>
-                {link.title}
-              </Link>
+              <motion.div key={link.title} variants={listItemVariants}>
+                <Link href={link.url}>{link.title}</Link>
+              </motion.div>
             ))}
-          </div>
+          </motion.div>
         )}
       </div>
     </div>
