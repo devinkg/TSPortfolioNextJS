e678f525e9fb8e8abcc698826ec8706f21bab769
Devin Godage <37541590+devinkg@users.noreply.github.com>
2024-03-25 12:52:39 +0530

    missing return added

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,14 +3,16 @@ import React from "react";
 import { motion } from "framer-motion";
 
 const ContactPage = () => {
-  <motion.div
-    className="h-full"
-    initial={{ y: "-200vh" }}
-    animate={{ y: "0%" }}
-    transition={{ duration: 1 }}
-  >
-    <div>ContactPage</div>
-  </motion.div>;
+  return (
+    <motion.div
+      className="h-full"
+      initial={{ y: "-200vh" }}
+      animate={{ y: "0%" }}
+      transition={{ duration: 1 }}
+    >
+      <div>ContactPage</div>
+    </motion.div>
+  );
 };
 
 export default ContactPage;
